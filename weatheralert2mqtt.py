04ed46213396097b44e8195d779b78824ca0d5fd
Dennis Sell <github@sellfamily.us>
2013-02-04 02:19:11 -0600

    Reworked to function as a daemon

--- weatheralert2mqtt.py
+++ weatheralert2mqtt.py
@@ -12,204 +12,76 @@ __author__ = "Dennis Sell"
 __copyright__ = "Copyright (C) Dennis Sell"
 
 
+APPNAME = "weatheralert2mqtt"
+VERSION = "0.7"
+WATCHTOPIC = "/raw/" + APPNAME + "/command"
 
-import os
-import sys
-import mosquitto
-import time
-import logging
-import signal
-import threading
-from config import Config
+
 from weatheralerts import nws
-import commands
+import time
+import subprocess
+from daemon import Daemon
+from mqttcore import MQTTClientCore
+from mqttcore import main
+import threading
+
+class MyMQTTClientCore(MQTTClientCore):
+    def __init__(self, appname, clienttype):
+        MQTTClientCore.__init__(self, appname, clienttype)
+        self.clientversion = VERSION
+        self.watchtopic = WATCHTOPIC
+        self.workingdir = self.cfg.WORKINGDIR
+        self.counties = self.cfg.COUNTIES
+        self.interval = self.cfg.INTERVAL
+        self.basetopic = self.cfg.BASE_TOPIC
+    
+        t = threading.Thread(target=self.do_thread_loop)
+        t.start()
+
+    def on_connect(self, mself, obj, rc):
+        MQTTClientCore.on_connect(self, mself, obj, rc)
+        self.mqttc.subscribe(self.watchtopic, qos=2)
+
+    def on_message(self, mself, obj, msg):
+        MQTTClientCore.on_message(self, mself, obj, msg)
+        if (msg.topic == self.watchtopic):
+            try:
+                print msg.payload
+                filename = self.workingdir + msg.payload + ".mp3"
+                pid = subprocess.Popen('mplayer -softvol ' +
+                                        filename, shell=True).pid
+            except:
+                print "Unknown command"
+
+    def do_thread_loop(self):
+        alerts = nws.Alerts()
+        while ( self.running ):
+		    if ( self.mqtt_connected ):
+			    for location in self.counties:
+				    print "Querrying for ", location.county, " county ", location.state
+				    try:
+#qos set wrong!!! broken TODO	
+                        result = "Failed to retreive"
+					    result = alerts.activefor_county(location)
+					    self.mqttc.publish( self.basetopic + location.state + "/" + location.county + "/alert", str(result), qos = 0, retain = 1 )
+				    except:
+					    print "error in weatheralerts."
+				    self.mqttc.publish( self.basetopic + location.state + "/" + location.county + "/time", time.strftime( "%x %X" ), qos = 0, retain = 1)
+			    if ( self.interval ):
+				    print "Waiting ", self.interval, " minutes for next update."
+				    time.sleep(60 * self.interval)
+			    else:
+				    self.running = False	#do a single shot
+				    print "Querries complete."
+		    pass
 
 
-CLIENT_NAME = "weatheralert2mqtt"
-CLIENT_VERSION = "0.6"
-MQTT_TIMEOUT = 60	#seconds
+class MyDaemon(Daemon):
+    def run(self):
+        mqttcore = MyMQTTClientCore(APPNAME, clienttype="type1")
+        mqttcore.main_loop()
 
 
-#TODO might want to add a lock file
-#TODO  need to deal with no config file existing!!!
-#TODO move config file to home dir
-
-
-#read in configuration file
-homedir = os.path.expanduser("~")
-f = file(homedir + '/.weatheralert2mqtt.conf')
-cfg = Config(f)
-MQTT_HOST = cfg.MQTT_HOST
-MQTT_PORT = cfg.MQTT_PORT
-CLIENT_TOPIC = cfg.CLIENT_TOPIC
-BASE_TOPIC = cfg.BASE_TOPIC
-COUNTIES = cfg.COUNTIES
-INTERVAL = cfg.INTERVAL
-
-
-mqtt_connected = 0
-
-
-#define what happens after connection
-def on_connect(self, obj, rc):
-	global mqtt_connected
-	global running
-	global alerts
-
-	mqtt_connected = True
-	print "MQTT Connected"
-	mqttc.publish( CLIENT_TOPIC + "status" , "online", 1, 1 )
-	mqttc.publish( CLIENT_TOPIC + "version", CLIENT_VERSION, 1, 1 )
-	ip = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
-	mqttc.publish( CLIENT_TOPIC + "ip", ip, 1, 1 )
-	mqttc.publish( CLIENT_TOPIC + "pid", os.getpid(), 1, 1 )
-	mqttc.subscribe( CLIENT_TOPIC + "ping", 2)
-
-
-def do_weatheralert_loop():
-	global running
-	global COUNTIES
-	global mqttc
-
-	while ( running ):
-		if ( mqtt_connected ):
-			for location in COUNTIES:
-				req_county = location.county
-				req_state = location.state
-				print "Querrying for ", req_county, " county ", req_state
-				try:
-					alerts.activefor_county(location)
-					result = alerts.activefor_county(location)
-					mqttc.publish( BASE_TOPIC + req_state + "/" + req_county + "/alert", str(result), qos = 2, retain = 1 )
-				except:
-					print "error in weatheralerts."
-
-				mqttc.publish( BASE_TOPIC + req_state + "/" + req_county + "/time", time.strftime( "%x %X" ), qos = 2, retain = 1)
-			if ( INTERVAL ):
-				print "Waiting ", INTERVAL, " minutes for next update."
-				time.sleep(60 * INTERVAL)
-			else:
-				running = False	#do a single shot
-				print "Querries complete."
-		pass
-
-
-def mqtt_connect():
-
-	rc = 1
-	while ( rc ):
-		print "Attempting connection..."
-		mqttc.will_set(CLIENT_TOPIC + "status", "disconnected", 1, 1)
-
-		#define the mqtt callbacks
-		mqttc.on_message = on_message
-		mqttc.on_connect = on_connect
-#		mqttc.on_disconnect = on_disconnect
-
-		#connect
-		rc = mqttc.connect( MQTT_HOST, MQTT_PORT, MQTT_TIMEOUT )
-		if rc != 0:
-			logging.info( "Connection failed with error code $s, Retrying in 30 seconds.", rc )
-			print "Connection failed with error code ", rc, ", Retrying in 30 seconds." 
-			time.sleep(30)
-		else:
-			print "Connect initiated OK"
-
-
-def on_message(self, obj, msg):
-	if (( msg.topic == CLIENT_TOPIC + "ping" ) and ( msg.payload == "request" )):
-		mqttc.publish( CLIENT_TOPIC + "ping", "response", qos = 1, retain = 0 )
-
-
-def do_disconnect():
-		global connected
-		mqttc.disconnect()
-		connected = 0
-		print "Disconnected"
-		mqttc.publish ( "/clients/" + CLIENT_NAME + "/status" , "offline", 1, 1 )
-
-
-#TODO are these redundant?????????????
-
-def mqtt_disconnect():
-	global mqtt_connected
-	print "Disconnecting..."
-	mqttc.disconnect()
-	if ( mqtt_connected ):
-		mqtt_connected = False 
-		print "MQTT Disconnected"
-
-
-def mqtt_connect():
-
-	rc = 1
-	while ( rc ):
-		print "Attempting connection..."
-		mqttc.will_set(CLIENT_TOPIC + "status", "disconnected_", 1, 1)
-
-		#define the mqtt callbacks
-		mqttc.on_message = on_message
-		mqttc.on_connect = on_connect
-#		mqttc.on_disconnect = on_disconnect
-
-		#connect
-		rc = mqttc.connect( MQTT_HOST, MQTT_PORT, MQTT_TIMEOUT )
-		if rc != 0:
-			logging.info( "Connection failed with error code $s, Retrying in 30 seconds.", rc )
-			print "Connection failed with error code ", rc, ", Retrying in 30 seconds." 
-			time.sleep(30)
-		else:
-			print "Connect initiated OK"
-
-
-
-def cleanup(signum, frame):
-	mqtt_disconnect()
-	sys.exit(signum)
-
-
-alerts = nws.Alerts()
-
-#create a client
-mqttc = mosquitto.Mosquitto( CLIENT_NAME ) 
-
-#trap kill signals including control-c
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-running = True
-
-t = threading.Thread(target=do_weatheralert_loop)
-t.start()
-
-
-def main_loop():
-	global mqtt_connected
-	mqttc.loop(10)
-	while running:
-		if ( mqtt_connected ):
-			rc = mqttc.loop(10)
-			if rc != 0:	
-				mqtt_disconnect()
-				print rc
-				print "Stalling for 20 seconds to allow broker connection to time out."
-				time.sleep(20)
-				mqtt_connect()
-				mqttc.loop(10)
-		pass
-
-
-mqtt_connect()
-main_loop()
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    daemon = MyDaemon('/tmp/' + APPNAME + '.pid')
+    main(daemon)
